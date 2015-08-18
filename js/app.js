1a9b46e892ce4f0ece1931620575b20149ccd524
willowcheng <e.wingus@Gmail.com>
2015-08-18 14:49:43 -0400

    Add select menu

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 //Problem: It look gross in smaller browser widths and small devices
 //Solution: To hide the text links and swap them out with a more appropriate navigation
 
-//Create a select and append to menu
+//Create a select and append to #menu
+var $select = $("<select></select>");
+$("#menu").append($select);
 //Cycle over menu links
+$("#menu a").each(function() {
+  var $anchor = $(this);
   //Create an option
+  var $option = $("<option></option>");
+
   //option's value is the href
+  $option.val($anchor.attr("href"));
   //option's text is the text of links
+  $option.text($anchor.text());
   //append option to select
+  $select.append($option);
+});
 //Create button
 //Bind click to button
   //Go to select's location
