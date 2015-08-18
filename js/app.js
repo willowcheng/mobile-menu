90cecd62a7a4344c7d62be06c4ff2ab86041714c
willowcheng <e.wingus@Gmail.com>
2015-08-18 14:40:23 -0400

    Perform

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 //Problem: It look gross in smaller browser widths and small devices
 //Solution: To hide the text links and swap them out with a more appropriate navigation
+
+//Create a select and append to menu
+//Cycle over menu links
+  //Create an option
+  //option's value is the href
+  //option's text is the text of links
+  //append option to select
+//Create button
+//Bind click to button
+  //Go to select's location
+//Modify CSS to hide links on small resolution and show button and select
+  //Also hides select and button on larger resolution and show's links
+//Deal with selected options depending on current page
